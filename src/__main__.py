bd68cde1bdf7496a80923bad27bb8c942e2f9784
Andrey Marakulin <annndruha.github@gmail.com>
2023-02-15 02:12:12 +0300

    Add help and start

--- src/__main__.py
+++ src/__main__.py
@@ -4,7 +4,6 @@
 import logging
 
 from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, MessageHandler
-from telegram.ext.filters import ALL, TEXT
 from telegram.ext import filters
 
 from src.settings import get_settings
@@ -19,9 +18,8 @@ logging.basicConfig(
 if __name__ == '__main__':
     settings = get_settings()
     application = ApplicationBuilder().token(settings.BOT_TOKEN).build()
-    # application.add_handler(CommandHandler('start', handler_start))
-    # application.add_handler(CommandHandler('help', handler_help))
+    application.add_handler(CommandHandler('start', handler_start))
+    application.add_handler(CommandHandler('help', handler_help))
     application.add_handler(CallbackQueryHandler(handler_button))
-    # application.add_handler(MessageHandler(ALL, handler_message))
     application.add_handler(MessageHandler(filters.Entity("mention"), handler_message))
     application.run_polling()
--- src/answers.py
+++ src/answers.py
@@ -1,3 +1,6 @@
+# Marakulin Andrey https://github.com/Annndruha
+# 2023
+
 ans = {
     'issue_open': '<b>Issue open by {}  via <a href="https://github.com/Annndruha/issue-github-telegram-bot">'
                   'Issue Telegram Bot</a></b>\n\n',
@@ -6,5 +9,10 @@ ans = {
     'assign_change': '\n\n**Assign changed from {} to {} by {} via '
                      '<a href="https://github.com/Annndruha/issue-github-telegram-bot">Issue Telegram Bot</a>**',
     'issue_close': '\n\n**Issue closed by {} via <a href="https://github.com/Annndruha/issue-github-telegram-bot'
-                   '">Issue Telegram Bot</a>**'
+                   '">Issue Telegram Bot</a>**',
+    'help': 'Чтобы создать issue, упомяните меня и после упоминания введите название issue.'
+            'Если вы хотите указать описание, после название сделайте перенос строки'
+            'и напишите описание. Пример сообщения для создания issue: \n\n'
+            '{} Название issue\nДостаточно долгое и длинное описание.'
+            '\nНастолько длинное, что занимает много строк.'
 }
