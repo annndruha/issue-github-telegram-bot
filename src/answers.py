a66f7950454f1cc61f0b834c1a6be35453f73415
Andrey Marakulin <annndruha.github@gmail.com>
2023-03-16 13:23:52 +0300

    Ссылки надоели

--- src/answers.py
+++ src/answers.py
@@ -2,14 +2,11 @@
 # 2023
 
 ans = {
-    'issue_open': '<b>Issue open by {}  via <a href="https://github.com/Annndruha/issue-github-telegram-bot">'
-                  'Issue Telegram Bot</a></b>\n\n',
+    'issue_open': '<b>Issue open by {}  via Issue Telegram Bot</a></b>\n\n',
     'link': '''<a href="{}">{}</a>''',
     'member_to_login': '''<a href="https://github.com/{}">{}</a>''',
-    'assign_change': '\n\n**Assign changed from {} to {} by {} via '
-                     '<a href="https://github.com/Annndruha/issue-github-telegram-bot">Issue Telegram Bot</a>**',
-    'issue_close': '\n\n**Issue closed by {} via <a href="https://github.com/Annndruha/issue-github-telegram-bot'
-                   '">Issue Telegram Bot</a>**',
+    'assign_change': '\n\n**Assign changed from {} to {} by {} via Issue Telegram Bot**',
+    'issue_close': '\n\n**Issue closed by {} via Issue Telegram Bot**',
     'help': 'Чтобы создать issue, упомяните меня и после упоминания введите название issue.'
             '\nЕсли вы хотите указать описание, после названия issue сделайте перенос строки'
             ' и напишите описание. Пример сообщения для создания issue: \n\n'
